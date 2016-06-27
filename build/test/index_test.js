8c916c8c450cabbae5ca94d1af121ed25551fcf9
Dylan Falconer <me@falconerd.com>
2016-06-28 02:45:31 +1000

    Cleaned

--- build/test/index_test.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-var tap = require("tap");
-var index_1 = require("../src/index");
-tap.pass("this is fine");
-tap.equal(4, index_1.SomeClass.add(1, 3));
--- src/index.js
+++
@@ -1,9 +0,0 @@
-var SomeClass = (function () {
-    function SomeClass() {
-    }
-    SomeClass.add = function (a, b) {
-        return a + b;
-    };
-    return SomeClass;
-})();
-exports.SomeClass = SomeClass;
