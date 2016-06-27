36dc178c35b8ec6a80b2583ac845a8bd9f30017b
Dylan Falconer <me@falconerd.com>
2016-06-28 05:13:50 +1000

    Added isValid logic

--- src/command-handler.ts
+++ src/command-handler.ts
@@ -1,5 +1,16 @@
 export class CommandHandler {
+  static commands = {
+    "add": true,
+    "remove": true,
+    "token": true,
+    "help": true,
+  };
+
   static isValid(input: string): boolean {
+    const command: Array<string> = input.split(" ");
+    if (command[0] !== "!dbg") return false;
+    if (!CommandHandler.commands[command[1]]) return false;
+    if (command.length > 3) return false;
     return true;
   }
 
