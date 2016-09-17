16065468cb00fb821a9304609af25e984d2f0213
me@falconerd.com <me@falconerd.com>
2016-09-17 16:23:22 +1000

    Removed build/bundle.js from cached

--- build/bundle.js
+++
@@ -1,7 +0,0 @@
-'use strict';
-
-function _interopDefault (ex) { return (ex && (typeof ex === 'object') && 'default' in ex) ? ex['default'] : ex; }
-
-var Discord = _interopDefault(require('discord.js'));
-
-console.log(Discord);
