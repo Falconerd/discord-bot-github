e704cc0848de6a6305807cb815f4454e5b6c7566
Dave R <davetheprogrammer2@gmail.com>
2017-10-07 15:24:29 +0100

    sendMessage is deprecated

--- src/commands.js
+++ src/commands.js
@@ -12,23 +12,23 @@ export default class Commands {
       // If the repo is private it will always return 404
       // So lets add it in good faith anyway.
       return Actions.add(repo, channel.id)
-      .then(result => channel.sendMessage(result))
+      .then(result => channel.send(result))
       .catch(error => {
         console.error('ERROR:', error);
-        channel.sendMessage('Something went wrong. An error has been logged.')
+        channel.send('Something went wrong. An error has been logged.')
       });
     } else {
       request(`https://github.com/${repo}`, (err, res) => {
         if (res.statusCode === 200) {
           return Actions.add(repo, channel.id)
-          .then(result => channel.sendMessage(result))
+          .then(result => channel.send(result))
           .catch(error => {
             console.error('ERROR:', error);
-            channel.sendMessage('Something went wrong. An error has been logged.')
+            channel.send('Something went wrong. An error has been logged.')
           });
         }
         if (res.statusCode === 404) {
-          channel.sendMessage('Repository not found.');
+          channel.send('Repository not found.');
         }
       });
     }
@@ -36,10 +36,10 @@ export default class Commands {
 
   static remove(channel, repo, _private) {
     return Actions.remove(repo, channel.id)
-    .then(result => channel.sendMessage(result))
+    .then(result => channel.send(result))
     .catch(error => {
       console.error('ERROR:', error);
-      channel.sendMessage('Something went wrong. An error has been logged.')
+      channel.send('Something went wrong. An error has been logged.')
     });
   }
 
