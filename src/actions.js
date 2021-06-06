bf7ae88801d9718e3558b85d60ece5310e1cab72
Dylan Falconer <me@falconerd.com>
2021-06-06 17:16:15 +1000

    Revert "Revert "Non-existing subscription error (#77)" (#81)"
    This reverts commit ef7f3b8b4a12ae8d412aaa9686f31f3c4b9cd2a7.

--- src/actions.js
+++ src/actions.js
@@ -1,8 +1,21 @@
 import { MongoClient } from "mongodb";
-import config from "./config";
 
 export default class Actions {
-  // @TODO Check if the subscription exists and return that.
+
+  /**
+   * Checks if the specific repoName is inside the subscriptions database
+   * @param {string} repoName Repository name
+   * @param {MongoClient} mongo_client MongoDB Client Instance
+   * @returns {boolean} Whether the repository exists or not
+   */
+
+  static async doesExist(repoName, mongo_client) {
+    const db = mongo_client.db("discobot");
+    const collection = db.collection("subscriptions");
+
+    const query = { repo: repoName.toLowerCase() };
+    return await collection.findOne(query);
+  }
   static add(repo, channelId, mongo_client) {
     return new Promise(async (resolve, reject) => {
       const db = mongo_client.db("discobot");
@@ -27,6 +40,9 @@ export default class Actions {
     return new Promise(async (resolve, reject) => {
       const db = mongo_client.db("discobot");
       const collection = db.collection("subscriptions");
+
+      const doesExist = await this.doesExist(repo, mongo_client);
+      if(!doesExist) reject(new Error('Not an existing subscription.'));
 
       const query = { repo: repo.toLowerCase(), channelId };
       await collection.deleteOne(query);
