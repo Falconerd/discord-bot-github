69d7d1f6d4bdf22ace15aa64ef538a62fb16e666
Dylan Falconer <me@falconerd.com>
2021-09-04 04:11:35 +1000

    Fix 86, also actually fix 64
    Users can now specify whether to display previews or not.
    
    Previous #64 fix didn't replace all.

--- src/commands.ts
+++ src/commands.ts
@@ -1,10 +1,18 @@
 import { Collection } from "mongodb";
 
+type Record = {
+  repo: string;
+  channelId: string;
+  secret?: string;
+  flags?: number;
+};
+
 export async function add(
   repo: string,
   channelId: string,
   subscriptions: Collection,
-  secret?: string | null
+  secret?: string | null,
+  previews?: boolean | null
 ): Promise<boolean> {
   const exists = await subscriptions.findOne({ repo, channelId });
 
@@ -12,11 +20,18 @@ export async function add(
     return false;
   }
 
+  const record: Record = { repo, channelId };
+
   if (secret) {
-    await subscriptions.insertOne({ repo, channelId, secret });
-  } else {
-    await subscriptions.insertOne({ repo, channelId });
+    record.secret = secret;
   }
+
+  if (previews) {
+    record.flags = 1;
+  }
+
+  await subscriptions.insertOne(record);
+
   return true;
 }
 
@@ -45,6 +60,12 @@ export const commands = [
         name: "secret",
         description: "Webhook secret (this will be stored as plain text)",
         type: 3,
+        required: false,
+      },
+      {
+        name: "previews",
+        description: "Display link previews",
+        type: 5,
         required: false,
       },
     ],
